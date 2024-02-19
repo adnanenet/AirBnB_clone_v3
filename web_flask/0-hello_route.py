0da8589f3935452b593bfea0f74cefb282caebd4
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-02-19 02:55:26 +0100

    All tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""Simple Flask web application"""
+"""Starts a Simple Flask web application
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+        /: Displays 'Hello HBNB!'
+"""
 from flask import Flask
-app = Flask('web_flask')
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
